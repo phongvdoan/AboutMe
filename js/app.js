479d1855f89bf19a40d77962d8989255c26f601a
Phong Doan <phongdoan.dev@gmail.com>
2019-11-07 16:28:42 -0800

    Redid guessing game

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ yesAnswerPersonalQues(kids);
 
 var instrument = prompt('Do you play a musical instrument?').toUpperCase();
 // console.log('Plays an instrument: ', instrument);
-yesAnswerPersonalQues(instrument);  
+yesAnswerPersonalQues(instrument);
 
 var maritalStatus = prompt('Are you married?').toLowerCase();
 // console.log('Is married: ', maritalStatus);
@@ -23,7 +23,7 @@ yesAnswerPersonalQues(maritalStatus);
 
 var drinkBeer = prompt('Do you like beer?').toUpperCase();
 // console.log('Likes beer: ', drinkBeer);
-noAnswerPersonalQues(maritalStatus);
+noAnswerPersonalQues(drinkBeer);
 
 // start number guessing game
 gameGuess();
@@ -74,33 +74,41 @@ function gameGuess() {
       counter++;
       // eslint-disable-next-line use-isnan
     } else if (guess === NaN || guess === null) {
-     alert('That is not a number. Input a number.');
+      alert('That is not a number. Input a number.');
       counter++;
     } else if (guess === 2) {
-     alert('CONGRATS! You have guess Phong\'s favorite number with in ' + counter + ' tries!');
+      alert('CONGRATS! You have guess Phong\'s favorite number with in ' + counter + ' tries!');
       correct++;
       break;
     }
-}
+  }
 
-alert('The correct answer was ' + favNum);
-} 
+  alert('The correct answer was ' + favNum);
+}
 
 // function plays guessing game with fav food
 function foodGame() {
   var food = ['salad', 'porkchops', 'chocolate', 'buffalo wings', 'tacos', 'sushi'];
   var foodGuess;
+  var count = 0;
+  var flag;
 
-  for (var i = 0; i < 6 && foodGuess !== food[5]; i++) {
-    foodGuess = prompt('What is Phong\'s favorite food? Pick from the following ' + food).toLowerCase();
-
-    if (foodGuess === food[5]) {
-      alert('Great job! Sushi is Phong\'s favorite food!');
-      correct++;
-      break;
-    } else {
-      alert('No, ' + foodGuess + ' is not Phong\'s favorite food.');
+  while (count < 6 && flag !== true) {
+    foodGuess = prompt('What is Phong\'s favorite food? You have ' + (6 - count) + ' tries left.').toLowerCase();
+    count++;
+    for (var i = 0; i < food.length; i++) {
+      if (foodGuess === food[i]) {
+        alert('Great job! Sushi is Phong\'s favorite food!');
+        correct++;
+        flag = true;
+      }
     }
+    alert('No, ' + foodGuess + ' is not Phong\'s favorite food.');
+  }
+  if (flag === false){
+    alert('You did not guess correctly within 6 tries!');
+  }
+  alert('My favorite foods include: ' + food);
 }
 alert('You scored ' + correct + 'out of 7!');
-}
+
